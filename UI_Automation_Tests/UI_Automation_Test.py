8850a628f92a8b6f8c322952dad4a39e7a051559
IsraelTruman <60275953+IsraelTruman@users.noreply.github.com>
2020-02-01 19:54:31 +0200

    Update UI_Automation_Test.py

--- UI_Automation_Tests/UI_Automation_Test.py
+++ UI_Automation_Tests/UI_Automation_Test.py
@@ -50,9 +50,9 @@ def download_game_message():
         download_button = driver.find_element_by_link_text("Download game")
         download_button.click()
         text_message = driver.find_element(By.XPATH, "//*[@id='pretend-modal']/div/div/div[2]").get_attribute(
-            'textContent')
+            'textContent').strip().replace("\n", "")
         print(str(now.strftime("%Y-%m-%d %H:%M:%S")) + " Download Game message is:")
-        print(str(now.strftime("%Y-%m-%d %H:%M:%S")) + " \"" + text_message.strip().replace("\n", "") + "\".")
+        print(str(now.strftime("%Y-%m-%d %H:%M:%S")) + " \"" + text_message + "\".")
     except TimeoutException:
         print(str(now.strftime("%Y-%m-%d %H:%M:%S")) + " Couldn't find 'Download Game' button element.")
         driver.quit()
